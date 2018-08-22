b43d6c2a49ded6c09cb8ced71921048b3d53661f
Man Van Hau <zentgroup.dev>
2018-08-22 17:50:11 +0700

    giao ho so cho hoi dong

--- app/Models/CouncilMissionScienceTechnology.php
+++ app/Models/CouncilMissionScienceTechnology.php
@@ -8,5 +8,5 @@ class CouncilMissionScienceTechnology extends Model
 {
     protected $table = 'council_mission_science_technologies';
 
-    protected $fillable = ['council_id', 'mission_science_technology_id'];
+    protected $fillable = ['council_id', 'mission_id'];
 }
