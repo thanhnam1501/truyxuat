d23e259c576fdb9c55d1e04ce12babeb53f8c86f
Man Van Hau <zentgroup.dev>
2018-08-23 14:08:56 +0700

    sua model Danh gia

--- app/Models/EvaluationForm.php
+++ app/Models/EvaluationForm.php
@@ -4,10 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class EvaluationFormScienceTechnology extends Model
+class EvaluationForm extends Model
 {
     protected $table = 'evaluation_form_science_technologies';
-    protected $fillable = ['user_id', 'mission_id', 'content'];
+    protected $fillable = ['user_id', 'mission_id', 'content', 'table_name'];
     protected $casts = [
         'content' => 'array',
     ];
