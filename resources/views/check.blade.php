1d4bfe5898c741a0ec29916ded06b7cb2ea5158f
Thanh Nam <thanhnam1501@gmail.com>
2019-02-13 15:09:13 +0700

    fix view check

--- resources/views/check.blade.php
+++ resources/views/check.blade.php
@@ -24,48 +24,61 @@
 
   <script src="{{asset('public/js/jquery.min.js')}}"></script>
 
-    <!--[if lt IE 9]>
-        <script src="../assets/js/ie8-responsive-file-warning.js"></script>
-      <![endif]-->
 
-      <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-        <![endif]-->
-        <style type="text/css">
-        .imageProduct{
-          width: 50%;
-          height: 50%; 
-          display: block;
-          margin-left: auto;
-          margin-right: auto;
-        }
-        .content img{
-         max-width: 100%;
-         max-height: 300px;
-       }
-       body{
-        background-color: white;
-        text-align: justify;
-        max-width: 100%;
-      }
-      .FirstContent{
-        height: 100%;
-        max-width: 100%;
+  <style type="text/css">
 
-        z-index: 1;
-        top: 0;
-        overflow-x: hidden;
-        padding-top: 20px;
-      }
-    }
-  </style>
+  @font-face {
+    font-family: Lora;
+    src: '../fonts/lora-v12-latin-regular.woff)';        
+  }
+
+  body{
+   font-family: "Lora";
+   background-color: white;
+   color: #000000;
+   text-align: justify;
+   width: 100%;
+   margin:0 auto;
+   padding: 0 auto;
+   font-size: 14px;
+ }
+ .imageProduct{
+  width: 100%;
+  height: 100%; 
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+.content img{
+ max-width: 100%;
+ max-height: 100%;
+ margin: 0 auto;
+ border-radius: 5%;
+}
+.content {    
+  width: 100%;
+}
+
+.x_panel{
+  border: none !important;
+}
+.btn{
+  margin-right: 0px !important;
+}
+
+iframe{
+  width: 100%;
+  margin: 0 auto;
+  padding: 0 auto;
+}
+/* lora-regular - latin */
+
+
+</style>
 </head>
 
-<body >
+<body>
 
- <div class="FirstContent" style="width: 100%;margin-right: 0 !important;">
   <div class="x_panel">
     <img class="imageProduct" src="{{asset('public/image/logo.jpg')}}" alt="">
     <br>
@@ -82,68 +95,63 @@
     @endif
   </div>
 
-
   <div class="x_panel">
-    <h4 style="text-align: center;"><strong>{{$data->name}}</strong></h4>
+    <h4 style="text-align: center; "><strong>{{$data->name}}</strong></h4>
   </div>
 
-  <div class="row">
-    <legend class="panel-heading container" style="background-color: #e0384f; width: 100%; text-align: center; justify-content: center; ">
-
-     <a href="#sort-content" style="background-color: #e0384f; width: 100%; margin: 0 auto !important; padding: auto; float: left"  class="btn panel-title fieldset-legend collapse-link" data-toggle="collapse" aria-expanded="true">
+  <div>
+    <div>
+     <a href="#sort-content" style="background-color: #338841; width: 100%"  class="btn panel-title fieldset-legend collapse-link" data-toggle="collapse" aria-expanded="true">
       <i class="fa fa-qrcode" style="color: #fff; float: left; margin-left: 5%; margin-right: 5%; margin-top: 1%"></i>
-      {{--   <span class="fieldset-legend-prefix element-invisible"">          
-      </span> --}}
+
       <span style="color: #fff !important; ">
         <strong style="float: left;">THÔNG TIN SẢN PHẨM</strong>
         <i class="ChangeIcon fa fa-chevron-up" style="float: right; margin-right: 5%;"></i>
       </span>
     </a>
-  </legend>
-  <div class="panel-body panel-collapse fade collapse in" id="sort-content" aria-expanded="true" style="">
-    <div id="sort-content" class="btn-group ">
-
-      {!!$data->sort_content!!}
-    </div>
   </div>
+
+  <div class="panel-body panel-collapse fade collapse in" id="sort-content" aria-expanded="true" style="">
+    <div id="sort-content" class="btn-group content">
+
+     <p class="standard">{!!$data->sort_content!!}</p>
+   </div>
+ </div>
 </div>
+
+
 
 @if($data->content !== null)
-<div class="row">
+<div>
+  <a href="#content" style="background-color: #338841; width: 100%"  class="btn panel-title fieldset-legend collapse-link" data-toggle="collapse" aria-expanded="true">
+    <span class="fieldset-legend-prefix element-invisible""></span>
+    <i class="fa fa-search" style="color: #fff; float: left; margin-left: 5%; margin-right: 5%; margin-top: 1%"></i>
+    <span style="color: #fff !important; ">
+      <strong style="float: left;">THÔNG TIN CHI TIẾT</strong>
+      <i class=" ChangeIcon fa fa-chevron-down" style="float: right;margin-right: 5%;"></i></span>
+    </a>
 
-  <legend class="panel-heading container" style="background-color: #82898f;">
-    <a href="#content" style="background-color: #82898f; width: 100%"  class="btn panel-title fieldset-legend collapse-link" data-toggle="collapse" aria-expanded="true">
-      <span class="fieldset-legend-prefix element-invisible""></span>
-      <i class="fa fa-search" style="color: #fff; float: left; margin-left: 5%; margin-right: 5%; margin-top: 1%"></i>
-      <span style="color: #fff !important; ">
-        <strong style="float: left;">THÔNG TIN CHI TIẾT</strong>
-        <i class=" ChangeIcon fa fa-chevron-down" style="float: right;margin-right: 5%;"></i></span>
-      </a>
-    </legend>
     <div class="panel-body panel-collapse fade collapse" id="content" aria-expanded="true" style="">
       <div id="content" class="btn-group content ">
-        {!!$data->content!!}
-      </div>
-    </div>
-  </div>
-</div>
-@endif
+       <p> {!!$data->content!!}</p>
+     </div>
+   </div>
+ </div>
+ @endif
 
 
 @for($i= 0; $i < $data->node ; $i++)
 @foreach($nodes as $key => $value)
 @if($key == $i && $value['status'] == 1)
-<div class="row">
-  <legend class="panel-heading container" style="background-color: #82898f; max-width: 100%">
-    <a href="#content{{$i}}" style="background-color: #82898f; width: 100%"  class="btn panel-title fieldset-legend collapse-link" data-toggle="collapse" aria-expanded="true">
-     <div class="iconfirst">  <span><i class="fa fa-tag" style="color: #fff; float: left; margin-left: 5%; margin-right: 5%; margin-top: 1%"></i></span></div>
-     <span style="color: #fff !important; "><strong style="float: left;">{{mb_strtoupper($value->name,'utf8')}}</strong><i class="ChangeIcon fa fa-chevron-down" style="float: right; margin-right: 5%;"></i></span>
-   </a>
- </legend>
+<div >
+  <a href="#content{{$i}}" style="background-color: #338841; width: 100%"  class="btn panel-title collapse-link" data-toggle="collapse" aria-expanded="true">
+   <div class="iconfirst">  <span><i class="fa fa-tag" style="color: #fff; float: left; margin-left: 5%; margin-right: 5%; margin-top: 1%"></i></span></div>
+   <span style="color: #fff !important; "><strong style="float: left;">{{mb_strtoupper($value->name,'utf8')}}</strong><i class="ChangeIcon fa fa-chevron-down" style="float: right; margin-right: 5%;"></i></span>
+ </a>
  <div class="panel-body panel-collapse fade collapse " id="content{{$i}}" aria-expanded="true" style="">
   <div id="content" class="btn-group content">
-    {!!$value->content!!}
-  </div>
+   <p> {!!$value->content!!}</p>
+ </div>
 </div>
 </div>
 @endif
@@ -151,20 +159,19 @@
 @endfor
 
 {{--  nhật ký sản xuất --}}
-<div class="row">
-  <legend class="panel-heading container" style="background-color: #82898f; max-width: 100%">
-    <a href="#process" style="background-color: #82898f; width: 100%"  class="btn panel-title fieldset-legend collapse-link" data-toggle="collapse" aria-expanded="true">
-     <div class="iconfirst">  <span><i class="fa fa-tag" style="color: #fff; float: left; margin-left: 5%; margin-right: 5%; margin-top: 1%"></i></span></div>
-     <span style="color: #fff !important; "><strong style="float: left;">NHẬT KÝ SẢN XUẤT</strong><i class="ChangeIcon fa fa-chevron-down" style="float: right; margin-right: 5%;"></i></span>
-   </a>
- </legend>
+<div >
+  <a href="#process" style="background-color: #338841; width: 100%"  class="btn panel-title fieldset-legend collapse-link" data-toggle="collapse" aria-expanded="true">
+   <div class="iconfirst">  <span><i class="fa fa-tag" style="color: #fff; float: left; margin-left: 5%; margin-right: 5%; margin-top: 1%"></i></span></div>
+   <span style="color: #fff !important; "><strong style="float: left;">NHẬT KÝ SẢN XUẤT</strong><i class="ChangeIcon fa fa-chevron-down" style="float: right; margin-right: 5%;"></i></span>
+ </a>
+ 
  <div class="panel-body panel-collapse fade collapse " id="process" aria-expanded="true" style="">
-  <div id="content" class="btn-group content">
+  <div id="process" class="btn-group content">
 
     @foreach($process as $key => $value)
     @if($value['status'] == 1)
-      <legend style="color: #fff;background-color: #7fbe42;padding: 0 auto;"><strong>{{$value->name}}</strong></legend>
-      <div>{!!$value->content!!} </div>
+    <legend style="color: #fff;background-color: #7fbe42;padding: 0 auto;"><strong>{{$value->name}}</strong></legend>
+    <div><p>{!!$value->content!!} </p></div>
     @endif
     @endforeach
 
@@ -184,7 +191,7 @@
 <h4 style="text-align: center;color: red;">Sản phẩm không có trong hệ thống của chúng tôi!</h4>
 @endif
 
-<div class="row">
+
  <div class="x_panel">
   <ul style="list-style: none;">
     <li>
@@ -210,8 +217,9 @@
 
  </ul>
 </div>
-</div>
-</div>
+
+
+
 
 <script src="{{asset('public/js/bootstrap.min.js')}}"></script>
 <script src="{{asset('public/js/chartjs/chart.min.js')}}"></script>
